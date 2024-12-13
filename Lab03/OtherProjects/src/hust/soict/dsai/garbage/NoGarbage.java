7761c5f40a66f9e9aafa5dfd9a3b48f055600979
letamquang-ai <letamquang220805@gmail.com>
2024-12-13 22:13:15 +0700

    Adding new Project

---
+++ Lab03/OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
@@ -0,0 +1,24 @@
+package hust.soict.dsai.garbage;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class NoGarbage {
+
+	public static void main(String[] args) throws IOException {
+		String filename = "D:\\Java_Eclipse\\OtherProjects\\input\\test.txt";
+		byte[] inputBytes = {0};
+		long startTime, endTime;
+		
+		inputBytes = Files.readAllBytes(Paths.get(filename));
+		startTime = System.currentTimeMillis();
+		StringBuilder outputStringBuilder = new StringBuilder();
+		for (byte b : inputBytes) {
+			outputStringBuilder.append((char)b);
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime-startTime);
+	}
+
+}
